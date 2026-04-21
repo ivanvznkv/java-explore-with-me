c3dc2d39e330e61f1956fd931065f0a39bbb9224
Vanek_Vyaznikov <ivn.vznkv@yandex.ru>
2026-04-21 17:50:36 +0300

    fix42

--- ewm-service/src/main/java/ru/practicum/ewm/event/dto/PublicEventSearchParams.java
+++ ewm-service/src/main/java/ru/practicum/ewm/event/dto/PublicEventSearchParams.java
@@ -3,7 +3,6 @@ package ru.practicum.ewm.event.dto;
 import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.PositiveOrZero;
-import lombok.Builder;
 import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 import ru.practicum.ewm.event.util.SortType;
@@ -12,7 +11,6 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
-@Builder
 public class PublicEventSearchParams {
     private String text;
     private List<Long> categories;
@@ -23,18 +21,13 @@ public class PublicEventSearchParams {
 
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime rangeEnd;
-
-    @Builder.Default
     private Boolean onlyAvailable = false;
-
     private SortType sort;
 
     @PositiveOrZero
-    @Builder.Default
     private int from = 0;
 
     @Positive
-    @Builder.Default
     private int size = 10;
 
     @AssertTrue(message = "Дата начала не может быть позже даты окончания")
